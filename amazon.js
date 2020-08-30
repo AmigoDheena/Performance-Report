93ff7b06b78aee9ebe4ccb57851c56cf41be4763
AmigoDheena <amigodheena@gmail.com>
2020-08-30 20:08:44 +0530

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -1,14 +1,35 @@
 const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch({headless: false}); // default is true
+const fs = require('fs');
+const search_term = 'ps4+games'
+const url = 'https://www.amazon.in/s?k='+search_term;
+const selector = '.s-include-content-margin';
+(async function(){
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-        deviceScaleFactor: 1,
+    await page.goto(url);
+    const product = await page.$$eval(selector, nodes => {
+        return nodes.map(node => {
+            const title = node.querySelector('.a-size-medium').textContent;
+            const price = document.querySelector('span .a-offscreen').textContent;
+            const rating = document.querySelector('.aok-align-bottom span').textContent;
+            const img = node.querySelector('.s-image').getAttribute('src');
+            return {
+                title,
+                price,
+                img,
+                rating
+            }
+        })
     });
-    await page.goto('https://www.amazon.in/');
-    await page.screenshot({path: 'amazon.png'}); 
+
+    //Save file as JSON
+    const jsonContent = JSON.stringify(product);
+    fs.writeFile("./myjsonfile.json", jsonContent, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+    console.log(product);
     await browser.close();
 })();
